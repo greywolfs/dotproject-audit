0249d3be83e42017cbbb34113ddeedf1f3ab1c6b
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2011-09-04 22:41:52 -0300

    New functions added to Auditor Class

--- audit.class.php
+++ audit.class.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('DP_BASE_DIR')) {
-	die('You should not access this file directly.');
+    die('You should not access this file directly.');
 }
 
 require_once $AppUI->getSystemClass('dp');
@@ -9,19 +9,22 @@ require_once $AppUI->getSystemClass('dp');
  */
 class Audit extends CDpObject {
 
-		function doStuff() {
-				return true;
-		}
+    function doStuff() {
+        return true;
+    }
 }
 
 class Auditor extends CDpObject {
 
+    var $contact_id = NULL;
+    var $project_id = NULL;
+
     function Auditor($perm_name='') {
-		$this->_tbl = 'auditors';
-		$this->_tbl_key = 'auditor_id';
-		$this->_permission_name = (($perm_name) ? $perm_name : 'auditors');
-		$this->_query = new DBQuery;
-	}
+        $this->_tbl = 'auditors';
+        $this->_tbl_key = 'auditor_id';
+        $this->_permission_name = (($perm_name) ? $perm_name : 'auditors');
+        $this->_query = new DBQuery;
+    }
 
     function loadAll($order = null, $where = null) {
         $this->_query->clear();
@@ -37,6 +40,37 @@ class Auditor extends CDpObject {
         $sql = $this->_query->prepare();
         $this->_query->clear();
         return db_loadHashList($sql, $this->_tbl_key);
+    }
+
+    function save() {
+        global $contact_id, $project_id;
+        if ($this->$contact_id != NULL || $this->$project_id != NULL){
+            $this->_query->clear();
+            $this->_query->addTable('auditors');
+            $this->_query->addInsert('project_id',$contact_id);
+            $this->_query->addInsert('contact_id',$project_id);
+            $this->_query->prepare();
+            $this->_query->clear();
+            return true;
+        }
+        return false;
+    }
+
+    function find($auditor_id){
+        $this->_query->clear();
+        $this->_query->addTable('auditors');
+        $this->_query->addWhere('auditor_id = '.$auditor_id);
+        $this->_query->prepare();
+        $this->_query->clear();
+        return $this->loadObject;
+    }
+
+    function delete($auditor_id){
+        $this->_query->clear();
+        $this->_query->setDelete('auditors');
+        $this->_query->addWhere('auditor_id = '.$auditor_id);
+        $this->_query->exec();
+        $this->_query->clear();
     }
 }
 ?>
