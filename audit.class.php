fb2a40917c2d0876738e57d1ae6d1add11016ccd
Rafael Silva <rsiloliveira@gmail.com>
2011-09-22 12:54:25 -0300

    Changes in interaction with the database

--- audit.class.php
+++ audit.class.php
@@ -19,57 +19,56 @@ class Auditor extends CDpObject {
     var $contact_id = NULL;
     var $project_id = NULL;
 
-    function Auditor($perm_name='') {
-        $this->_tbl = 'auditors';
-        $this->_tbl_key = 'auditor_id';
-        $this->_permission_name = (($perm_name) ? $perm_name : 'auditors');
-        $this->_query = new DBQuery;
+    function Auditor() {
+        $this->CDpObject('auditors', 'auditor_id');
     }
 
     function loadAll($order = null, $where = null) {
-        $this->_query->clear();
-        $this->_query->addTable('auditors','auditors');
+        $q = new DBQuery();
+        $q->clear();
+        $q->addTable('auditors','auditors');
         if ($order) {
-            $this->_query->addOrder($order);
+            $q->addOrder($order);
         }
         if ($where) {
-            $this->_query->addWhere($where);
+            $q->addWhere($where);
         }
-        $this->_query->addJoin('projects', 'projects', 'projects.project_id = auditors.project_id');
-        $this->_query->addJoin('contacts', 'contacts', 'contacts.contact_id = auditors.contact_id');
-        $sql = $this->_query->prepare();
-        $this->_query->clear();
+        $q->addJoin('projects', 'projects', 'projects.project_id = auditors.project_id');
+        $q->addJoin('contacts', 'contacts', 'contacts.contact_id = auditors.contact_id');
+        $sql = $q->prepare();
         return db_loadHashList($sql, $this->_tbl_key);
     }
 
     function save() {
+        $q = new DBQuery();
+        $q->clear();
         if ($this->contact_id != NULL && $this->project_id != NULL){
-            $this->_query->clear();
-            $this->_query->addTable('auditors');
-            $this->_query->addInsert('project_id',$this->project_id);
-            $this->_query->addInsert('contact_id',$this->contact_id);
-            $this->_query->exec();
-            $this->_query->clear();
+            $q->addTable('auditors');
+            $q->addInsert('project_id',$this->project_id);
+            $q->addInsert('contact_id',$this->contact_id);
+            $q->prepare();
             return true;
         }
         return false;
     }
 
     function find($auditor_id){
-        $this->_query->clear();
-        $this->_query->addTable('auditors');
-        $this->_query->addWhere('auditor_id = '.$auditor_id);
-        $this->_query->exec();
-        $this->_query->clear();
+        $q = new DBQuery();
+        $q->clear();
+        $q->addTable('auditors');
+        $q->addWhere('auditor_id = '.$auditor_id);
+        $q->prepare();
         return $this->loadObject;
     }
 
-    function delete($auditor_id){
-        $this->_query->clear();
-        $this->_query->setDelete('auditors');
-        $this->_query->addWhere('auditor_id = '.$auditor_id);
-        $this->_query->exec();
-        $this->_query->clear();
+    function remove($auditor_id){
+        $q = new DBQuery();
+        $q->setDelete('auditors');
+        $q->addWhere('auditor_id = ' . $auditor_id);
+        if (!$q->exec())
+			return db_error();
+		else
+			return null;
     }
 }
 ?>
