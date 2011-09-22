0e3dbc89ce9bbc7f83c59f5b5d7048fdee383231
Rafael Silva <rsiloliveira@gmail.com>
2011-09-22 12:56:13 -0300

    Remove Function Added

--- projects_tab.auditors.php
+++ projects_tab.auditors.php
@@ -6,6 +6,21 @@ if (!defined('DP_BASE_DIR')) {
 GLOBAL $AppUI, $project_id;
 require_once($AppUI->getModuleClass('audit'));
 
+$auditor_id = intval(dPgetParam($_GET, 'auditor_id', 0));
+
+if($auditor_id){
+   if (getPermission('audit', 'delete')){
+       if($auditor_id){
+            $obj_auditor = new Auditor();
+           // nao esta utilizando o delete de API do DP que seria
+           // $obj_auditor->delete($auditor_id);
+            $obj_auditor->remove($auditor_id);
+        }
+
+    }
+}
+
+
 if (getPermission('audit', 'view')) {
     $a = new Auditor();
     $list = $a->loadAll();
@@ -13,7 +28,7 @@ if (getPermission('audit', 'view')) {
         <table class="std" width="100%">
             <tbody>
                 <tr>
-                    <td align="right"><a href="?m=audit&amp;a=add_auditor&amp;project_id=<?= $project_id ?>"><?= $AppUI->_('Add')?></a></td>
+                    <td align="right"><a href="?m=audit&amp;a=add_auditor&amp;project_id=<?php echo $project_id; ?>"><?php echo $AppUI->_('Add');?></a></td>
                 </tr>
             </tbody>
         </table>
@@ -21,19 +36,19 @@ if (getPermission('audit', 'view')) {
     <table class="tbl" width="100%">
             <thead>
                 <tr>
-                    <th><?= $AppUI->_('Last Name')?></th>
-                    <th><?= $AppUI->_('First Name')?></th>
-                    <th><?= $AppUI->_('Email')?></th>
-                    <th><?= $AppUI->_('Remove')?></th>
+                    <th><?php echo $AppUI->_('Last Name');?></th>
+                    <th><?php echo $AppUI->_('First Name');?></th>
+                    <th><?php echo $AppUI->_('Email');?></th>
+                    <th><?php echo $AppUI->_('Remove');?></th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach($list as $item){?>
                 <tr>
-                    <td><?= $item['contact_last_name'] ?></td>
-                    <td><?= $item['contact_first_name'] ?></td>
-                    <td><?= $item['contact_email'] ?></td>
-                    <td><a href="?m=audit&amp;a=delete_auditor&amp;auditor_id=<?= $item['auditor_id'] ?>"><?= $AppUI->_('Remove')?></a></td>
+                    <td><?php echo $item['contact_last_name']; ?></td>
+                    <td><?php echo $item['contact_first_name']; ?></td>
+                    <td><?php echo $item['contact_email']; ?></td>
+                    <td><a href="?m=projects&amp;a=view&amp;project_id=<?php echo $project_id; ?>&amp;tab=5&amp;auditor_id=<?php echo $item['auditor_id']; ?>"><?php echo $AppUI->_('Remove');?></a></td>
                 </tr>
                 <?php } ?>
             </tbody>
