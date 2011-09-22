d10f33f6fa4aad0460df382133af64f2c1d9fcfe
Rafael Silva <rsiloliveira@gmail.com>
2011-09-22 17:06:41 -0300

    Artefact Class Added

--- audit.class.php
+++ audit.class.php
@@ -70,5 +70,31 @@ class Auditor extends CDpObject {
 		else
 			return null;
     }
+
 }
+    class Artefact extends CDpObject {
+
+        var $project_id = NULL;
+        var $artefact_id = NULL;
+        var $artefact_name = NULL;
+        var $artefact_short_description = NULL;
+        var $artefact_description = NULL;
+        var $artefact_phase = NULL;
+        var $artefact_status = NULL;
+
+        function Artefact() {
+            $this->CDpObject('artefacts', 'artefact_id');
+        }
+
+        function remove($artefact_id){
+        $q = new DBQuery();
+        $q->setDelete('artefacts');
+        $q->addWhere('artefact_id = ' . $artefact_id);
+        if (!$q->exec())
+			return db_error();
+		else
+			return null;
+    }
+
+    }
 ?>
